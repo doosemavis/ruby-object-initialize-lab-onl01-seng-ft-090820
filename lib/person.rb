23c725461f70fd637a57edd5529cbc109fb3ef0f
Moose Davis <moosedavis2011@gmail.com>
2020-09-17 18:39:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,15 @@
 class Person
   
   def initialize("Beyonce")
-    @person = "Beyonce"
+    @name = "Beyonce"
   end 
   
-  def person=(person)
-    @person = person
+  def name=(name)
+    @name = name
   end 
   
-  def person
-    @person
+  def name
+    @name 
   end 
 end 
 
