10c75b5139b75599ab1893073b6dfec5b91815a5
Moose Davis <moosedavis2011@gmail.com>
2020-09-17 18:43:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,17 @@
 class Dog 
+  
+  def initialize(dogs_name)
+    @name = dogs_name
+  end 
+  
+  def name(dogs_name)
+    @name = dogs_name
+  end 
+  
+  def name
+    @name
+  end 
 end 
+
+fido = Dog.new("Fido")
+fido.name
